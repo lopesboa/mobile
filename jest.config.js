9a0cb768a98a3b42ca18268fbe487d9b40bde15b
Vincent CATILLON <contact@vincent-catillon.fr>
2018-11-30 15:01:33 +0100

    Implements coverage (#23)
    * Implements coverage
    
    * Fix tests

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,22 @@ module.exports = {
   ],
   testPathIgnorePatterns: ['node_modules', '/__fixtures__/', '__mocks__'],
   setupFiles: ['./jest-setup.js'],
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text', 'html'],
+  coveragePathIgnorePatterns: ['node_modules', 'jest-setup.js', 'src/*.js'],
+  coverageThreshold: {
+    'src/components': {
+      branches: 100,
+      functions: 90,
+      lines: 100,
+      statements: 90,
+    },
+    'src/modules': {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  // verbose: true,
 };
