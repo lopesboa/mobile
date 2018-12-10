90921cf161eb12aaee620caa55b9de343b9d149f
Vincent CATILLON <contact@vincent-catillon.fr>
2018-12-10 10:36:51 +0100

    Disable landscape mode on smartphones (#27)
    * Disable landscape mode on smartphones
    
    * Fix some config

--- android/app/src/main/java/com/coorpacademy/app/MainActivity.java
+++ android/app/src/main/java/com/coorpacademy/app/MainActivity.java
@@ -1,11 +1,22 @@
 package com.coorpacademy.app;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getBoolean(R.bool.portrait_only)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
