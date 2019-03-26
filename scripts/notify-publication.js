cbd5089d77ee01c0433bde71404617de71feb7cd
Adrien Becchis <adrien.becchis@gmail.com>
2019-03-26 18:52:27 +0100

    Notifying publications to slack (#214)
    * Scaffold slack notify script :speech_balloon:
    
    * Accept message from the command line :inbox_tray:
    
    * Basic improvement of notifier script to handle color and title :rainbow:
    
    * Have fastlane invoking the broadcast slack event :satellite:

---
+++ scripts/notify-publication.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console, unicorn/no-process-exit */
+const {WebClient} = require('@slack/client');
+
+const token = process.env.SLACK_TOKEN;
+if (!token) {
+  console.log("Pas d'bras, Pas d'chocolat! 😉");
+  process.exit(1);
+}
+const channel = process.env.SLACK_CHANNEL;
+if (!channel) {
+  console.log('Mais à qui voulez vous parler? 😉');
+  process.exit(3);
+}
+const web = new WebClient(token);
+
+(async () => {
+  const res = await web.chat.postMessage({
+    channel,
+    attachments: [
+      {
+        text:
+          process.argv[2] ||
+          'Someone want to tell us something, but he did not tell what! :postbox:',
+        color: process.argv[3] || 'warning',
+        title: process.argv[4]
+      }
+    ]
+  });
+  console.log('Message sent: ', res.ts);
+})();
