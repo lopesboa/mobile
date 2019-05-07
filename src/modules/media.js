35bd036d53765145cbd99bc17c80046ad437a459
BertrandD <bernaz@pm.me>
2019-05-07 09:37:38 +0200

    handle jwt video (#282)
    * handle jwt video
    
    * improve test

--- src/modules/media.js
+++ src/modules/media.js
@@ -1,14 +1,21 @@
 // @flow
 
-import type {Media, LessonType} from '@coorpacademy/progression-engine';
+import type {Media, LessonType, Lesson} from '@coorpacademy/progression-engine';
 import {RESOURCE_TYPE} from '../const';
-import {getCleanUri} from './uri';
+import {getCleanUri, getResourceUrl} from './uri';
 
-export const getMediaType = (mediaItem: Media): LessonType | void => {
+export const getMediaType = (mediaItem: Media | Lesson): LessonType | void => {
   return mediaItem.type;
 };
-export const getMediaUrl = (mediaItem: Media): string | void => {
+export const getMediaUrl = (mediaItem: Media | Lesson): string | void => {
   if (getMediaType(mediaItem) === RESOURCE_TYPE.VIDEO) {
+    if (mediaItem.videoId) {
+      // here flow cannot determine for sure if the mediaItem is either  a mediaItem or a Lesson.
+      // this is mainly due to `getQuestionMedia` can return a Media or a Lesson
+      // $FlowFixMe;
+      const lesson: Lesson = mediaItem;
+      return getResourceUrl(lesson);
+    }
     const resourceItem = mediaItem.src && mediaItem.src[0];
     return resourceItem && getCleanUri(resourceItem.url);
   }
--- src/modules/media.test.js
+++ src/modules/media.test.js
@@ -6,10 +6,11 @@ import {getMediaUrl, getMediaPoster, getMediaType} from './media';
 describe('getMediaType', () => {
   it('should return the type -- VIDEO case ', () => {
     const result = getMediaType(video);
-    expect(result).toBeDefined();
+
+    expect(result).toEqual('video');
   });
 
-  it('should return the URL -- VIDEO case ', () => {
+  it('should return the type -- VIDEO case ', () => {
     const result = getMediaPoster({...image, type: undefined});
     expect(result).not.toBeDefined();
   });
@@ -18,18 +19,31 @@ describe('getMediaURl', () => {
   it('should return the URL -- IMG case ', () => {
     const result = getMediaUrl(image);
 
-    expect(result).toBeDefined();
+    expect(result).toEqual(
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-eyrolles/cockpit-eyrolles/default/shutterstock_123603871-1-1545058448041.jpg&h=500&w=500&q=90&m=contain'
+    );
   });
 
   it('should return the URL -- IMG case with src ', () => {
     const result = getMediaUrl(image);
-    expect(result).toBeDefined();
+
+    expect(result).toBe(
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-eyrolles/cockpit-eyrolles/default/shutterstock_123603871-1-1545058448041.jpg&h=500&w=500&q=90&m=contain'
+    );
   });
 
   it('should return the URL -- VIDEO case ', () => {
     const result = getMediaUrl(video);
 
-    expect(result).toBeDefined();
+    expect(result).toBe(
+      'https://player.vimeo.com/external/266296552.m3u8?s=316e71edf867847e1f648655833e41cce19da389&oauth2_token_id=411503075'
+    );
+  });
+
+  it('should return the URL -- VIDEO case ', () => {
+    const videoId = 'foo';
+    const result = getMediaUrl({...video, videoId: videoId});
+    expect(result).toEqual(`https://content.jwplatform.com/videos/${videoId}.mp4`);
   });
 
   it('should return the URL -- EMPTY MEDIA case ', () => {
@@ -39,14 +53,18 @@ describe('getMediaURl', () => {
 
   it('should return the URL -- PDF  case ', () => {
     const result = getMediaUrl(pdf);
-    expect(result).toBeDefined();
+    expect(result).toBe(
+      'https://api.coorpacademy.com/api-service/medias?url=https://static.coorpacademy.com/content/CoorpAcademy/content-eyrolles/cockpit-eyrolles/default/shutterstock_123603871-1-1545058448041.jpg&h=500&w=500&q=90&m=contain'
+    );
   });
 });
 
 describe('getMediaPoster', () => {
   it('should return the POSTER -- VIDEO case ', () => {
     const result = getMediaPoster(video);
-    expect(result).toBeDefined();
+    expect(result).toBe(
+      'https://player.vimeo.com/external/266296552.m3u8?s=316e71edf867847e1f648655833e41cce19da389&oauth2_token_id=411503075'
+    );
   });
 
   it('should return the poster -- IMAGE case ', () => {
