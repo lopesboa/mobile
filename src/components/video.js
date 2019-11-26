147559ee1eb99cd7f9e08b928c1151f42b8ebc81
Vincent Catillon <contact@vincent-catillon.fr>
2019-11-26 10:44:02 +0100

    Update video component to test it

--- src/components/video.js
+++ src/components/video.js
@@ -104,122 +104,162 @@ const styles = StyleSheet.create({
   }
 });
 
-const Video = ({
-  source,
-  preview,
-  height,
-  step,
-  subtitles: subtitlesUri,
-  hasSubtitles,
-  isFullScreen,
-  onPlay,
-  onReady,
-  onEnd,
-  onExpand,
-  onShrink,
-  onSubtitlesToggle,
-  onRef,
-  onError,
-  onProgress,
-  testID,
-  extralifeOverlay = false
-}: Props) => {
-  const testIDSuffix = testID ? '-' + testID : '';
-  const testIDFullscreenSuffix =
-    (testID && (isFullScreen ? '-fullscreen' + testIDSuffix : testIDSuffix)) ||
-    (isFullScreen ? '-fullscreen' : '');
+class Video extends React.PureComponent<Props> {
+  props: Props;
 
-  const containerHeight = (!isFullScreen && height) || undefined;
-  const subtitles: Array<Subtitles> = Platform.OS === 'ios' ? [EMPTY_SUBTITLES] : [];
-  if (subtitlesUri) {
-    subtitles.push({
-      title: 'Subtitles',
-      language: translations.getLanguage(),
-      type: TextTrackType.VTT,
-      uri: subtitlesUri
-    });
-  }
-  const disabledSubtitles =
-    Platform.OS === 'ios'
-      ? {type: 'language', value: EMPTY_SUBTITLES.language}
-      : {type: 'disabled'};
-  const selectedSubtitles = hasSubtitles
-    ? {
-        type: 'language',
-        value: translations.getLanguage()
-      }
-    : disabledSubtitles;
+  webView = WebView | null;
 
-  return (
-    <View
-      style={[styles.container, isFullScreen && styles.fullScreen, {height: containerHeight}]}
-      testID={`video-container${testIDFullscreenSuffix}`}
-    >
-      {[STEP.PREVIEW, STEP.LOADING].includes(step) && (
-        <Preview
-          type={extralifeOverlay ? EXTRALIFE : RESOURCE_TYPE.VIDEO}
-          source={preview}
-          isLoading={step === STEP.LOADING}
-          onPress={onPlay}
-          testID={testID}
-        />
-      )}
-      {[STEP.PLAY, STEP.END].includes(step) && (
-        <React.Fragment>
-          <BlackPortal name="video">
-            <VideoPlayer
-              testID={`video${testIDFullscreenSuffix}`}
-              source={source}
-              ref={onRef}
-              style={styles.video}
-              resizeMode="contain"
-              disableVolume
-              disableBack
-              ignoreSilentSwitch="ignore"
-              disableFullscreen={Boolean(!onExpand && !onShrink)}
-              toggleResizeModeOnFullscreen={false}
-              isFullscreen={isFullScreen}
-              onEnterFullscreen={onExpand}
-              onExitFullscreen={onShrink}
-              onFullscreenPlayerWillDismiss={onShrink}
-              onEnd={onEnd}
-              onReadyForDisplay={onReady}
-              onError={onError}
-              onProgress={onProgress}
-              onCC={onSubtitlesToggle}
-              disableCC={!subtitlesUri}
-              textTracks={(subtitlesUri && subtitles) || undefined}
-              selectedTextTrack={(subtitlesUri && selectedSubtitles) || undefined}
-              isCC={hasSubtitles}
-            />
-            <WebView
-              source={{uri: 'https://test-adways.herokuapp.com/'}}
-              style={{backgroundColor: 'transparent'}}
-            />
-          </BlackPortal>
-          {(Platform.OS !== 'android' || !isFullScreen) && <WhitePortal name="video" />}
-        </React.Fragment>
-      )}
-      {[STEP.END, STEP.ERROR].includes(step) && (
-        <ResourceOverlay>
-          <Touchable
+  handleDataReceived = data => {
+    console.log('handleDataReceived', data);
+    data.isSuccessfull = true;
+    data.args = [data.data % 2 ? 'green' : 'red'];
+    this.webView.injectJavaScript(`window.postMessage('${JSON.stringify(data)}', '*');`);
+  };
+
+  handleWebViewMessage = event => {
+    let data;
+    try {
+      data = JSON.parse(event.nativeEvent.data);
+    } catch (err) {
+      console.warn(err);
+      return;
+    }
+
+    console.log('handleWebViewMessage', event, data);
+
+    switch (data.targetFunc) {
+      case 'handleDataReceived':
+        this.handleDataReceived(data);
+        break;
+    }
+  };
+
+  handleWebViewRef = (ref: WebView | null) => {
+    this.webView = ref;
+  };
+
+  render() {
+    const {
+      source,
+      preview,
+      height,
+      step,
+      subtitles: subtitlesUri,
+      hasSubtitles,
+      isFullScreen,
+      onPlay,
+      onReady,
+      onEnd,
+      onExpand,
+      onShrink,
+      onSubtitlesToggle,
+      onRef,
+      onError,
+      onProgress,
+      testID,
+      extralifeOverlay = false
+    } = this.props;
+
+    const testIDSuffix = testID ? '-' + testID : '';
+    const testIDFullscreenSuffix =
+      (testID && (isFullScreen ? '-fullscreen' + testIDSuffix : testIDSuffix)) ||
+      (isFullScreen ? '-fullscreen' : '');
+
+    const containerHeight = (!isFullScreen && height) || undefined;
+    const subtitles: Array<Subtitles> = Platform.OS === 'ios' ? [EMPTY_SUBTITLES] : [];
+    if (subtitlesUri) {
+      subtitles.push({
+        title: 'Subtitles',
+        language: translations.getLanguage(),
+        type: TextTrackType.VTT,
+        uri: subtitlesUri
+      });
+    }
+    const disabledSubtitles =
+      Platform.OS === 'ios'
+        ? {type: 'language', value: EMPTY_SUBTITLES.language}
+        : {type: 'disabled'};
+    const selectedSubtitles = hasSubtitles
+      ? {
+          type: 'language',
+          value: translations.getLanguage()
+        }
+      : disabledSubtitles;
+
+    return (
+      <View
+        style={[styles.container, isFullScreen && styles.fullScreen, {height: containerHeight}]}
+        testID={`video-container${testIDFullscreenSuffix}`}
+      >
+        {[STEP.PREVIEW, STEP.LOADING].includes(step) && (
+          <Preview
+            type={extralifeOverlay ? EXTRALIFE : RESOURCE_TYPE.VIDEO}
+            source={preview}
+            isLoading={step === STEP.LOADING}
             onPress={onPlay}
-            style={styles.replay}
-            testID={`video-${step}-replay-${testIDSuffix}`}
-            analyticsID={`video-${step}-replay`}
-          >
-            <NovaSolidDesignActionsRedo color={theme.colors.white} height={40} width={40} />
-            {step === STEP.ERROR && (
-              <React.Fragment>
-                <Space />
-                <Text style={styles.error}>{translations.videoLoadingError}</Text>
-              </React.Fragment>
-            )}
-          </Touchable>
-        </ResourceOverlay>
-      )}
-    </View>
-  );
-};
+            testID={testID}
+          />
+        )}
+        {[STEP.PLAY, STEP.END].includes(step) && (
+          <React.Fragment>
+            <BlackPortal name="video">
+              <VideoPlayer
+                testID={`video${testIDFullscreenSuffix}`}
+                source={source}
+                ref={onRef}
+                style={styles.video}
+                resizeMode="contain"
+                disableVolume
+                disableBack
+                ignoreSilentSwitch="ignore"
+                disableFullscreen={Boolean(!onExpand && !onShrink)}
+                toggleResizeModeOnFullscreen={false}
+                isFullscreen={isFullScreen}
+                onEnterFullscreen={onExpand}
+                onExitFullscreen={onShrink}
+                onFullscreenPlayerWillDismiss={onShrink}
+                onEnd={onEnd}
+                onReadyForDisplay={onReady}
+                onError={onError}
+                onProgress={onProgress}
+                onCC={onSubtitlesToggle}
+                disableCC={!subtitlesUri}
+                textTracks={(subtitlesUri && subtitles) || undefined}
+                selectedTextTrack={(subtitlesUri && selectedSubtitles) || undefined}
+                isCC={hasSubtitles}
+              />
+              <WebView
+                ref={this.handleWebViewRef}
+                // source={{uri: 'https://test-adways.herokuapp.com/'}}
+                source={require('../assets/html/adways.html')}
+                style={{backgroundColor: 'transparent'}}
+                onMessage={this.handleWebViewMessage}
+              />
+            </BlackPortal>
+            {(Platform.OS !== 'android' || !isFullScreen) && <WhitePortal name="video" />}
+          </React.Fragment>
+        )}
+        {[STEP.END, STEP.ERROR].includes(step) && (
+          <ResourceOverlay>
+            <Touchable
+              onPress={onPlay}
+              style={styles.replay}
+              testID={`video-${step}-replay-${testIDSuffix}`}
+              analyticsID={`video-${step}-replay`}
+            >
+              <NovaSolidDesignActionsRedo color={theme.colors.white} height={40} width={40} />
+              {step === STEP.ERROR && (
+                <React.Fragment>
+                  <Space />
+                  <Text style={styles.error}>{translations.videoLoadingError}</Text>
+                </React.Fragment>
+              )}
+            </Touchable>
+          </ResourceOverlay>
+        )}
+      </View>
+    );
+  }
+}
 
 export default Video;
