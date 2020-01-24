585dea0f41b1c6d1e252d25bdb837f5fce760192
Vincent Catillon <contact@vincent-catillon.fr>
2020-01-24 10:36:53 +0100

    Fix box shadow on catalog cards (#468)

--- src/components/card.js
+++ src/components/card.js
@@ -53,8 +53,8 @@ const Card = ({children, style, testID, type}: Props) => {
       );
     default:
       return (
-        <View style={styles.container} testID={testID}>
-          <View style={[style, BOX_STYLE, styles.overflowHidden]}>{children}</View>
+        <View style={[styles.container, BOX_STYLE]} testID={testID}>
+          <View style={[style, styles.overflowHidden]}>{children}</View>
         </View>
       );
   }
