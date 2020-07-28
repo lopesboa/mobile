7de90faa27386a85084f894ee57e51f0a75aa5b0
Alan Langlois <alan@etaminstudio.com>
2020-07-28 12:10:10 +0200

    removing zoOr lib

--- ios/Coorpacademy/AppDelegate.m
+++ ios/Coorpacademy/AppDelegate.m
@@ -8,8 +8,6 @@
 #import <React/RCTLinkingManager.h>
 #import "Firebase.h"
 
-#import <UserNotifications/UserNotifications.h>
-#import <RNCPushNotificationIOS.h>
 @import Firebase;
 
 #if DEBUG
@@ -79,10 +77,6 @@ static void InitializeFlipper(UIApplication *application) {
   // Splashscreen
   [RNSplashScreen show];
 
-    // Define UNUserNotificationCenter
-  UNUserNotificationCenter *center =
-      [UNUserNotificationCenter currentNotificationCenter];
-  center.delegate = self;
 
   return YES;
 }
@@ -91,56 +85,6 @@ static void InitializeFlipper(UIApplication *application) {
   return [Orientation getOrientation];
 }
 
-// Called when a notification is delivered to a foreground app.
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-       willPresentNotification:(UNNotification *)notification
-         withCompletionHandler:
-             (void (^)(UNNotificationPresentationOptions options))
-                 completionHandler {
-  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert |
-                    UNAuthorizationOptionBadge);
-}
-
-// Required to register for notifications
-- (void)application:(UIApplication *)application
-    didRegisterUserNotificationSettings:
-        (UIUserNotificationSettings *)notificationSettings {
-  [RNCPushNotificationIOS
-      didRegisterUserNotificationSettings:notificationSettings];
-}
-// Required for the register event.
-- (void)application:(UIApplication *)application
-    didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  [RNCPushNotificationIOS
-      didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-// Required for the notification event. You must call the completion handler
-// after handling the remote notification.
-- (void)application:(UIApplication *)application
-    didReceiveRemoteNotification:(NSDictionary *)userInfo
-          fetchCompletionHandler:
-              (void (^)(UIBackgroundFetchResult))completionHandler {
-  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo
-                                fetchCompletionHandler:completionHandler];
-}
-// Required for the registrationError event.
-- (void)application:(UIApplication *)application
-    didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-  [RNCPushNotificationIOS
-      didFailToRegisterForRemoteNotificationsWithError:error];
-}
-// Required for the localNotification event.
-- (void)application:(UIApplication *)application
-    didReceiveLocalNotification:(UILocalNotification *)notification {
-  [RNCPushNotificationIOS didReceiveLocalNotification:notification];
-}
-// IOS 10+ Required for local notification tapped event
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-    didReceiveNotificationResponse:(UNNotificationResponse *)response
-             withCompletionHandler:(void (^)(void))completionHandler {
-  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
-  completionHandler();
-}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
